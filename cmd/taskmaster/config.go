5e467e7180688da1581eebb3782339e975ae38eb
Alexander <bombblob@gmail.com>
2019-09-14 12:20:05 -0700

    Added config parsing and stuff

---
+++ cmd/taskmaster/config.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Cmd          string            `yaml:"cmd"`
+	NumProcs     int               `yaml:"numprocs"`
+	Umask        int               `yaml:"umask"`
+	WorkingDir   string            `yaml:"workingdir"`
+	AutoStart    bool              `yaml:"autostart"`
+	AutoRestart  string            `yaml:"autorestart"`
+	ExitCodes    interface{}       `yaml:"exitcodes"` // int or []int
+	StartRetries int               `yaml:"startretries"`
+	StartTime    int               `yaml:"starttime"`
+	StopSignal   string            `yaml:"stopsignal"`
+	StopTime     int               `yaml:"stoptime"`
+	Stdout       string            `yaml:"stdout"`
+	Stderr       string            `yaml:"stderr"`
+	Env          map[string]string `yaml:"env"`
+}
+
+func ConfParse(filename string) ([]Config, error) {
+	ymap := make(map[interface{}]interface{})
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	err = yaml.Unmarshal([]byte(data), &ymap)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	var confs []Config
+	for _, v := range ymap["programs"].(map[interface{}]interface{}) {
+		fmt.Println(v)
+		data, err := yaml.Marshal(v)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		conf := Config{}
+		err = yaml.Unmarshal(data, &conf)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		fmt.Printf("%+v\n", conf)
+		confs = append(confs, conf)
+		fmt.Println("")
+	}
+	return confs, nil
+}
--- cmd/taskmaster/taskmaster.go
+++ cmd/taskmaster/taskmaster.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	flag.Parse()
+	args := flag.Args()
+	fmt.Println(args)
+
+	if len(args) != 1 {
+		fmt.Errorf("Invalid number of args: %d", len(args))
+	}
+	confs, err := ConfParse(args[0])
+	if err != nil {
+		panic(err) // TODO: address error
+	}
+	for _, a := range confs {
+		fmt.Printf("%+v\n", a)
+	}
 }
